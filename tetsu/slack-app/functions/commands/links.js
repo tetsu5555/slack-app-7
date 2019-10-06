8edfc6a9f9702b96512d38553e0d685718c3b9ee
tetsu5555 <tetete1118@gmail.com>
2019-10-07 00:20:08 +0900

    feat: add attachment to /links command

--- tetsu/slack-app/functions/commands/links.js
+++ tetsu/slack-app/functions/commands/links.js
@@ -17,8 +17,41 @@ module.exports = (user, channel, text = '', command = {}, botToken = null, callb
   callback(null, {
     text: `show links`,
     attachments: [
-      // You can customize your messages with attachments.
-      // See https://api.slack.com/docs/message-attachments for more info.
+      {
+        "fallback": "Plan a vacation",
+        "author_name": "Owner: rdesoto",
+        "title": "Plan a vacation",
+        "text": "I've been working too hard, it's time for a break.",
+        "actions": [
+          {
+            "name": "action",
+            "type": "button",
+            "text": "Complete this task",
+            "style": "",
+            "value": "complete"
+          },
+          {
+            "name": "tags_list",
+            "type": "select",
+            "text": "Add a tag...",
+            "data_source": "static",
+            "options": [
+              {
+                "text": "Launch Blocking",
+                "value": "launch-blocking"
+              },
+              {
+                "text": "Enhancement",
+                "value": "enhancement"
+              },
+              {
+                "text": "Bug",
+                "value": "bug"
+              }
+            ]
+          }
+        ]
+      }
     ]
   });
 };
